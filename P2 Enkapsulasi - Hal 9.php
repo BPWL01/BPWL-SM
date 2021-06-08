e10c0f5d1689b6a5a8047d5c64867d447ecd0c93
Aisyah-Mumtazah <85545259+Aisyah-Mumtazah@users.noreply.github.com>
2021-06-08 14:04:38 +0700

    Commit Kedua

--- P2 Enkapsulasi - Hal 9.php
+++ P2 Enkapsulasi - Hal 9.php
@@ -7,7 +7,10 @@ class mei{
 
 	//method tampilkan_nama
 	function tampilkan_nama(){
-		return "Nama saya Aisyah Syahidatul Mumtazah";
+		return "Nama saya Aisyah Syahidatul Mumtazah Dari kelas 1TIB";
+	}
+	function latihan(){
+		return "Latihan Pake GitHub di BPWL 2:03 PM";
 	}
 }
 //instansiasi class mei
@@ -15,4 +18,5 @@ $mei = new mei();
 
 //memanggil method tampilkan_nama dari class mei
 echo $mei->tampilkan_nama();
+echo $mei->latihan();
 ?>
